21f36ff77f7e4b45dd255e070b105c21e2c7ac12
Emmanuel <emmanuelizu92@gmail.com>
2025-09-26 19:05:59 -0700

    arithmetic_operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,17 @@
+def perform_operation(num1,num2,operation):
+    match operation:
+        case 'add':
+            return num1+num2
+        
+        case 'subtract':
+            return num1-num2
+        
+        case 'multiply':
+            return num1*num2
+        
+        case 'divide':
+            try:
+                answer = num1/num2
+            except ZeroDivisionError as e:
+                return e
+            return answer
