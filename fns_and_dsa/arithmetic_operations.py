438c5a0ed57912e6d0bad0f46a853020ee51e67f
Emmanuel <emmanuelizu92@gmail.com>
2025-09-26 19:28:16 -0700

    arithmetic_operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,13 +2,13 @@ def perform_operation(num1, num2, operation):
     if operation == 'add':
         return num1+num2
         
-    if operation == 'subtract':
+    elif operation == 'subtract':
         return num1-num2
         
-    if operation == 'multiply':
+    elif operation == 'multiply':
         return num1*num2
         
-    if operation =='divide':
+    elif operation =='divide':
         try:
             answer = num1/num2
         except ZeroDivisionError as e:
