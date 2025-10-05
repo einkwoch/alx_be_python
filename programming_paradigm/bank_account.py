6f12d1114e91d0e87ec7e4f2bb8ead6a899c46cb
Emmanuel <emmanuelizu92@gmail.com>
2025-10-04 23:43:49 -0700

    Create a Simple Bank Account Class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,37 +1,19 @@
 class BankAccount:
-    BALANCE_FILE = "balance.txt"
+    def __init__ (self,account_balance):
+        self.account_balance = account_balance
 
-    def __init__(self, account_balance=None):
-        if account_balance is not None:
-            self.account_balance = account_balance
-        else:
-            # Load balance from file if it exists, otherwise start with 100
-            self.account_balance = self._load_balance()
+    def deposit(self,amount):
+        self.account_balance+=amount
 
-    def _load_balance(self):
-        if os.path.exists(self.BALANCE_FILE):
-            with open(self.BALANCE_FILE, "r") as f:
-                return float(f.read().strip())
-        return 100.0
-
-    def _save_balance(self):
-        with open(self.BALANCE_FILE, "w") as f:
-            f.write(str(self.account_balance))
-
-    def deposit(self, amount):
-        self.account_balance += amount
-        self._save_balance()
-
-    def withdraw(self, amount):
+    def withdraw(self,amount):
         if amount <= self.account_balance:
-            self.account_balance -= amount
-            self._save_balance()
+            self.account_balance-=amount
             return True
         else:
             return False
         
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f'Current Balance: $[{self.account_balance}]')
 
 
 bank = BankAccount(100)
